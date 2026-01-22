71699d6a249132236a64247fb8afa9281d648190
Akbarali Farkhodov <akbarfarkhodov@gmail.com>
2026-01-22 13:50:19 -0500

    Some new practices

---
+++ week-2/practice.js
@@ -0,0 +1,28 @@
+// primitive data types
+
+// 1. String
+
+let user = "Ali";
+console.log(user);
+
+// 2. number
+
+let userID = 6977;
+console.log(userID);
+
+// 3. boolean
+
+let isPractice = true;
+console.log(isPractice);
+
+// 4. null
+
+let userAdmin = null;
+userAdmin = "Ali";
+console.log(userAdmin);
+
+// 5. undefined
+
+let users = undefined;
+users = "Ali";
+console.log(users);
